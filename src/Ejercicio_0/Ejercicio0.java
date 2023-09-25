811394f1ebdb97878e87cc83de7bd8280b541eca
out0f4mm0 <eriking2004@gmail.com>
2023-09-25 10:34:06 +0200

    Exercise 0 done

---
+++ src/Ejercicio_0/Ejercicio0.java
@@ -0,0 +1,12 @@
+package Ejercicio_0;
+
+public class Ejercicio0{
+    public static void main(String[] args){
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i + " table:");
+            for(int j = 0; j <= 12; j++) {
+                System.out.println(i + " * "+ j + " = " + i * j);
+            }
+        }
+    }
+}
