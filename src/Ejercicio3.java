8d0da7ececb79ff6012c97998b12c83d8355bf1c
c0d3sh0rt4g3 <eriking2004@gmail.com>
2023-09-25 12:49:48 +0200

    Exercises 3 done

---
+++ src/Ejercicio3.java
@@ -0,0 +1,8 @@
+public class Ejercicio3 {
+    public static void main(String[] args){
+        String nombre = "Erik";
+        String direccion = "calle grajo";
+        String telefono = "698911320";
+        System.out.println("Hola, me llamo " + nombre + " vivo en la " + direccion + " y mi número de teléfono es el " + telefono);
+    }
+}
