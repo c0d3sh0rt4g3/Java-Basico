a090f7590603ddae7bc773b076bb442ff1fa1cc5
out0f4mm0 <eriking2004@gmail.com>
2023-09-28 13:15:26 +0200

    Exercise 7 of 01

---
+++ src/Ejercicios01/Ejercicio7.java
@@ -0,0 +1,14 @@
+package Ejercicios01;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please introduce a prodcut price without taxes:");
+        float taxBase = scanner.nextFloat();
+        float taxPercent = 0.21F;
+        float finalPrice = (taxBase * taxPercent) + taxBase;
+        System.out.println("The product price with taxes added would be " + finalPrice + " euros.");
+    }
+}
