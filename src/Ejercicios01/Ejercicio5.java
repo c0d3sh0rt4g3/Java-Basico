75d472fb9ae8d3172a03225614ae46f26b27e589
out0f4mm0 <eriking2004@gmail.com>
2023-09-28 13:06:21 +0200

    Minor changes on exercises 5 and 6 of 01

--- src/Ejercicios01/Ejercicio5.java
+++ src/Ejercicios01/Ejercicio5.java
@@ -1,9 +1,14 @@
 package Ejercicios01;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
     public static void main(String[] args){
-        float base = 5F;
-        float height = 7F;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce the rectangle base:");
+        float base = scanner.nextFloat();
+        System.out.println("Introduce the rectangle height:");
+        float height = scanner.nextFloat();
         float rectangleArea = base * height;
         System.out.println("The area of a rectangle with base " + base + " and height " + height + " woulde be " + rectangleArea);
     }
--- src/Ejercicios01/Ejercicio6.java
+++ src/Ejercicios01/Ejercicio6.java
@@ -1,9 +1,14 @@
 package Ejercicios01;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
     public static void main(String[] args){
-        float base = 5F;
-        float height = 7F;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce the triangle base:");
+        float base = scanner.nextFloat();
+        System.out.println("Introduce the triangle height:");
+        float height = scanner.nextFloat();
         float triangleArea = (base * height) % 2;
         System.out.println("The area of a triangle with base " + base + " and height " + height + " woulde be " + triangleArea);
     }
